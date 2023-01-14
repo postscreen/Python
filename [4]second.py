3750b96aa074ba4f874975e3ae57f07349f798fe
Sergey Morozov <berserker992@gmail.com>
2023-01-15 02:36:01 +1100

    Fourth homework 1.1

--- [4]second.py
+++ [4]second.py
@@ -8,11 +8,60 @@ import os
 import time
 import random
 
+def textToArchive(inputData):
+    prev = ""
+    count = 1
+    result = ""
+    for char in range(0,len(inputData)):
+        if prev != inputData[char]:
+            if prev != "":
+                if prev != "\n":
+                    result += "{1}{0}".format(prev,count)
+                else:
+                    result += "\n"
+                count = 1
+            prev = inputData[char]
+        else:
+            count += 1
+    return result + "{1}{0}".format(prev,count)
+
+def archiveToText(inputData):
+    result = ""
+    data = re.findall("([0-9]+)([a-zA-Z]+)|([\n])", inputData)
+    for elem in data:
+        if elem[2] != "\n":
+            result += (str(elem[1]) * int(elem[0]))
+        else:
+            result += elem[2]
+    return result
+
+os.system('cls')
+
+# Read text
 fInput = open("iRLE.txt", "r")
 inputData = fInput.read()
 fInput.close()
 
-print(inputData)
+# Transform text to archive
+archiveData = textToArchive(inputData)
 
-for char in range(0,len(inputData)):
-    print(inputData[char])
+# Save archive
+fOutput = open("oRLE.txt", "w")
+fOutput.write(archiveData)
+fOutput.close()
+
+# Return base text
+textFromTheArchive = archiveToText(archiveData)
+
+# Compare what they are equal
+print("Input text:")
+print(inputData)
+print()
+
+print("Restored text:")
+print(textFromTheArchive)
+print()
+
+print("Archive:")
+print(archiveData)
+print()
